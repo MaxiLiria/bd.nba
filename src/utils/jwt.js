29e75fc984c9ff7378a9feaa96b597f42e13d4af
Maxi <maaxiliria@gmail.com>
2023-06-22 12:12:21 +0200

    second commit

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateSign = (id, email) => {
-    return jwt.sign({id, email}, process.env.JWT_KEY, {expiresIn: '365d'})
+    return jwt.sign({id, email}, process.env.JWT_KEY, {expiresIn: '365h'})
 }
 
 
